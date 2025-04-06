3dedb8a60ab464735a75252e113a4fd12cc91632
Jҽϝϝɾҽყ ʂαɱα <jeffreymosesdj@gmail.com>
2025-04-07 02:34:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,5 @@ START_PIC = "https://i.ibb.co/ynjcqYdZ/photo-2025-04-06-20-48-47-749030498576734
 
 MONGO_URI = "mongodb+srv://soulmovies37:FwHMGCpSMCnVVmhw@cluster0.uyokx.mongodb.net/?retryWrites=true&w=majority" #Databse uri get it from https://www.mongodb.com/
 
-ADMINS = "6266529037" #Admin ID
-
 
 #By RexySama
