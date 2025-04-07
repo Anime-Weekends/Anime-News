b54179957cd2231e71635284990c499e588cdaa2
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-08 00:44:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 # config.py
 
-API_ID = 28744454
-API_HASH = "debd37cef0ad1a1ce45d0be8e8c3c5e7"
-BOT_TOKEN = "7773900178:AAEQEQAXahfYAVsol1EUcwYPKa0Or2cPCdg"
+import os
 
-OWNER_ID = 6266529037
-ADMINS = [6266529037]  # Additional admins will be added dynamically
+API_ID = int(os.getenv("API_ID", "28744454"))
+API_HASH = os.getenv("API_HASH", "debd37cef0ad1a1ce45d0be8e8c3c5e7")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your-bot-token")
+MONGO_URI = os.getenv("MONGO_URI", "your-mongo-uri")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://your-webhook-url.koyeb.app")
 
-MONGO_URI = "mongodb+srv://soulmovies37:FwHMGCpSMCnVVmhw@cluster0.uyokx.mongodb.net/?retryWrites=true&w=majority"
+START_PIC = os.getenv("START_PIC", "https://i.ibb.co/ynjcqYdZ/photo-2025-04-06-20-48-47-7490304985767346192.jpg")
 
-WEBHOOK_URL = "https://cheerful-kimberlee-filetolink1286-a70f100b.koyeb.app"
+OWNER_ID = int(os.getenv("OWNER_ID", "6266529037"))
+ADMINS = list(map(int, os.getenv("ADMINS", "6266529037").split(",")))
+
 URL_A = "https://myanimelist.net/rss.php?type=news"
-
-START_PIC = "https://i.ibb.co/ynjcqYdZ/photo-2025-04-06-20-48-47-7490304985767346192.jpg"
