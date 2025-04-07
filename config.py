849a48db47fcd9f77150b4d1ab652017e6db8fab
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-07 15:18:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-API_ID=28744454
-API_HASH=debd37cef0ad1a1ce45d0be8e8c3c5e7
-BOT_TOKEN=7773900178:AAEQEQAXahfYAVsol1EUcwYPKa0Or2cPCdg
-URL_A=http://myanimelist.net/rss.php?type=news
-START_PIC=https://i.ibb.co/ynjcqYdZ/photo-2025-04-06-20-48-47-7490304985767346192.jpg
-MONGO_URI=mongodb+srv://soulmovies37:FwHMGCpSMCnVVmhw@cluster0.uyokx.mongodb.net/?retryWrites=true&w=majority
-OWNER_ID=6266529037
+API_ID = 28744454
+API_HASH = "debd37cef0ad1a1ce45d0be8e8c3c5e7"
+BOT_TOKEN = "7773900178:AAEQEQAXahfYAVsol1EUcwYPKa0Or2cPCdg"
+URL_A = "http://myanimelist.net/rss.php?type=news"
+START_PIC = "https://i.ibb.co/ynjcqYdZ/photo-2025-04-06-20-48-47-7490304985767346192.jpg"
+MONGO_URI = "mongodb+srv://soulmovies37:FwHMGCpSMCnVVmhw@cluster0.uyokx.mongodb.net/?retryWrites=true&w=majority"
+OWNER_ID = 6266529037
 
 #By RexySama
