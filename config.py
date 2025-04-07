caed422f45b9a39ebd4c7e9f8b8466818749ae79
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-08 00:18:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,13 @@
 API_ID = 28744454
 API_HASH = "debd37cef0ad1a1ce45d0be8e8c3c5e7"
 BOT_TOKEN = "7773900178:AAEQEQAXahfYAVsol1EUcwYPKa0Or2cPCdg"
-WEBHOOK_URL = "https://cheerful-kimberlee-filetolink1286-a70f100b.koyeb.app"
-MONGO_URI = "mongodb+srv://soulmovies37:FwHMGCpSMCnVVmhw@cluster0.uyokx.mongodb.net/?retryWrites=true&w=majority"
-
-URL_A = "https://myanimelist.net/rss.php?type=news"
-START_PIC = "https://i.ibb.co/ynjcqYdZ/photo-2025-04-06-20-48-47-7490304985767346192.jpg"
 
 OWNER_ID = 6266529037
-ADMINS = [6266529037]
+ADMINS = [6266529037]  # Additional admins will be added dynamically
+
+MONGO_URI = "mongodb+srv://soulmovies37:FwHMGCpSMCnVVmhw@cluster0.uyokx.mongodb.net/?retryWrites=true&w=majority"
+
+WEBHOOK_URL = "https://cheerful-kimberlee-filetolink1286-a70f100b.koyeb.app"
+URL_A = "https://myanimelist.net/rss.php?type=news"
+
+START_PIC = "https://i.ibb.co/ynjcqYdZ/photo-2025-04-06-20-48-47-7490304985767346192.jpg"
