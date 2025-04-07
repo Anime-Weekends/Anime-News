20b62e8e99759abb04d5f8053978eded431e52d9
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-08 01:49:50 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,21 +1,14 @@
-# webhook.py
-
 from flask import Flask, request
-from bot import app as pyro_app
-from config import WEBHOOK_URL, BOT_TOKEN
+from bot import app  # This should be the Pyrofork Client
 
-app = Flask(__name__)
+server = Flask(__name__)
 
-@app.route(f"/{BOT_TOKEN}", methods=["POST"])
+@server.route("/", methods=["GET", "POST"])
 def webhook():
-    update = request.get_data()
-    pyro_app.process_webhook_update(update)
-    return "OK", 200
-
-@app.route("/", methods=["GET"])
-def root():
-    return "Bot is alive!", 200
+    if request.method == "POST":
+        app.process_update(request.get_json(force=True))
+        return "OK", 200
+    return "Running", 200
 
 if __name__ == "__main__":
-    pyro_app.set_webhook(WEBHOOK_URL + f"/{BOT_TOKEN}")
-    app.run(host="0.0.0.0", port=8080)
+    server.run(host="0.0.0.0", port=10000)
