30951d3b05a5c6087007bf919c5ce1fb6e93b10c
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-07 22:34:21 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,27 +1,21 @@
 from flask import Flask, request
 from pyrogram import Client
-from config import BOT_TOKEN, WEBHOOK_URL, API_ID, API_HASH
+from config import API_ID, API_HASH, BOT_TOKEN, WEBHOOK_URL
 
 app = Flask(__name__)
-
-bot = Client(
-    "AnimeNewsBot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-)
+bot = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 @app.route(f"/{BOT_TOKEN}", methods=["POST"])
 def webhook():
-    update = request.get_data()
-    bot.process_update(update)
-    return "OK", 200
+    update = request.get_json()
+    if update:
+        bot.process_update(update)
+    return "OK"
 
 @app.route("/", methods=["GET"])
 def home():
-    return "Bot is running with webhook", 200
+    return "Bot is running"
 
 if __name__ == "__main__":
     bot.start()
-    bot.set_webhook(WEBHOOK_URL + f"/{BOT_TOKEN}")
     app.run(host="0.0.0.0", port=8080)
