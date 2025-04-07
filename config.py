f0f18422333e7f5b4b22ebb4776f9564dac0e608
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-07 19:53:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ BOT_TOKEN = "7773900178:AAEQEQAXahfYAVsol1EUcwYPKa0Or2cPCdg"
 
 # IMPORTANT: This should be your public server URL, not an RSS feed.
 # Example: "https://your-app-name.vercel.app" or your own hosted domain.
-URL_A = "https://your-server.com"
+URL_A = "https://cheerful-kimberlee-filetolink1286-a70f100b.koyeb.app/"
 
 START_PIC = "https://i.ibb.co/ynjcqYdZ/photo-2025-04-06-20-48-47-7490304985767346192.jpg"
 MONGO_URI = "mongodb+srv://soulmovies37:FwHMGCpSMCnVVmhw@cluster0.uyokx.mongodb.net/?retryWrites=true&w=majority"
