963036da95fb32c7e2abec1c2ef6d0ede8456350
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-07 14:57:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,11 @@
-# config.py
+import os
 
-API_ID = "28744454" #API ID get it from telegram.org
-
-API_HASH = "debd37cef0ad1a1ce45d0be8e8c3c5e7" #API HASH get it from telegram.org
-
-BOT_TOKEN = "7773900178:AAEQEQAXahfYAVsol1EUcwYPKa0Or2cPCdg" #Bot Token get it from @BotFather
-
-URL_A = "http://myanimelist.net/rss.php?type=news" #News RSS Feed Url 2
-
-#URL_B = "" #News RSS Feed Url 2
-
-START_PIC = "https://i.ibb.co/ynjcqYdZ/photo-2025-04-06-20-48-47-7490304985767346192.jpg" #Startup Pic
-
-MONGO_URI = "mongodb+srv://soulmovies37:FwHMGCpSMCnVVmhw@cluster0.uyokx.mongodb.net/?retryWrites=true&w=majority" #Databse uri get it from https://www.mongodb.com/
-
+API_ID = int(os.environ.get("28744454"))
+API_HASH = os.environ.get("debd37cef0ad1a1ce45d0be8e8c3c5e7")
+BOT_TOKEN = os.environ.get("7773900178:AAEQEQAXahfYAVsol1EUcwYPKa0Or2cPCdg")
+URL_A = os.environ.get("http://myanimelist.net/rss.php?type=news")
+START_PIC = os.environ.get("https://i.ibb.co/ynjcqYdZ/photo-2025-04-06-20-48-47-7490304985767346192.jpg")
+MONGO_URI = os.environ.get("mongodb+srv://soulmovies37:FwHMGCpSMCnVVmhw@cluster0.uyokx.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = int(os.environ.get("OWNER_ID", 6266529037))
-ADMINS = [OWNER_ID]
 
 #By RexySama
