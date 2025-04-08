bf3cb4f63a97ed440fc461d1c1fce7779671f440
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-08 11:03:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,14 @@
-# config.py
-
 import os
+from dotenv import load_dotenv
 
-API_ID = int(os.getenv("API_ID", "28744454"))
-API_HASH = os.getenv("API_HASH", "debd37cef0ad1a1ce45d0be8e8c3c5e7")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your-bot-token")
-MONGO_URI = os.getenv("MONGO_URI", "your-mongo-uri")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://your-webhook-url.koyeb.app")
+load_dotenv()
 
-START_PIC = os.getenv("START_PIC", "https://i.ibb.co/ynjcqYdZ/photo-2025-04-06-20-48-47-7490304985767346192.jpg")
-
-OWNER_ID = int(os.getenv("OWNER_ID", "6266529037"))
+API_ID = int(os.getenv("28744454"))
+API_HASH = os.getenv("debd37cef0ad1a1ce45d0be8e8c3c5e7")
+BOT_TOKEN = os.getenv("7773900178:AAEQEQAXahfYAVsol1EUcwYPKa0Or2cPCdg")
+WEBHOOK_URL = os.getenv("")
+START_PIC = os.getenv("https://i.ibb.co/ynjcqYdZ/photo-2025-04-06-20-48-47-7490304985767346192.jpg")
+URL_A = os.getenv("https://myanimelist.net/rss.php?type=news")
+MONGO_URI = os.getenv("")
+OWNER_ID = int(os.getenv("6266529037"))
 ADMINS = list(map(int, os.getenv("ADMINS", "6266529037").split(",")))
-
-URL_A = "https://myanimelist.net/rss.php?type=news"
