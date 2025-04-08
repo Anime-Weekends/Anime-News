cfc825e153e9dc0e6599d9a06981849274e865fa
Rᴇxʏ sᴀᴍᴀ <jeffreymosesdj@gmail.com>
2025-04-08 11:04:17 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,14 +1,14 @@
-from flask import Flask, request
-from bot import app  # This should be the Pyrofork Client
+from flask import Flask, jsonify
 
-server = Flask(__name__)
+app = Flask(__name__)
 
-@server.route("/", methods=["GET", "POST"])
-def webhook():
-    if request.method == "POST":
-        app.process_update(request.get_json(force=True))
-        return "OK", 200
-    return "Running", 200
+@app.route("/")
+def root():
+    return jsonify("Auto Anime News Bot is running!")
 
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=10000)
+@app.route("/health")
+def health():
+    return jsonify({"status": "OK"})
+
+def start_webhook():
+    app.run(host="0.0.0.0", port=8000, threaded=True)
